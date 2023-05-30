ad8796caa8129bf064dfab4acc7b862582ff1d36
Jeremy Magland <jmagland@flatironinstitute.org>
2023-05-30 17:14:27 -0400

    start_compute_resource

--- python/stan_playground/cli.py
+++ python/stan_playground/cli.py
@@ -1,6 +1,6 @@
 import click
 import stan_playground
-from .start import start as start_function
+from .start_compute_resource import start as start_compute_resource_function
 
 
 @click.group(help="stan-playground command line interface")
@@ -11,15 +11,15 @@ def main():
 def register_compute_resource():
     print(stan_playground.register_compute_resource(dir='.'))
 
-@click.command()
-@click.option('--dir', required=True, help='The directory to share.')
-def start(dir: str):
-    start_function(dir)
+@click.command(help="Start the compute resource in the specified directory")
+@click.option('--dir', required=True, help='The directory of the compute resource.')
+def start_compute_resource(dir: str):
+    start_compute_resource_function(dir)
 
 @click.command(help='Initialize the singularity container')
 def initialize_singularity_container():
     stan_playground.initialize_singularity_container()
 
 main.add_command(register_compute_resource)
-main.add_command(start)
+main.add_command(start_compute_resource)
 main.add_command(initialize_singularity_container)
